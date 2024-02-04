70c5c2f08cf45724dcca23045c5ae36c5543b2a5
BlackSerperior6 <m330.nek76@gmail.com>
2024-02-04 20:13:02 +0500

    init

---
+++ Task1/Task1/Task1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int matrix[500][500];
+	int rows;
+	int columns;
+	int next_digit = 1;
+
+	cout << "Введите кол-во строчек и столбоцов" << endl;
+
+	cin >> rows >> columns;
+
+	if (rows < 1 || columns < 1)
+	{
+		cout << "Невалидные значения. Должны быть >= 1" << endl;
+		return 0;
+	}
+
+	cout << endl << "Ваша матрица: " << endl << endl;
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+
+			if ((i + j) % 2 != 0 )
+				matrix[i][j] = 0;
+			else
+			{
+				matrix[i][j] = next_digit;
+				next_digit++;
+
+				if (next_digit > 9)
+					next_digit = 1;
+			}
+
+			cout << matrix[i][j] << " ";
+		}
+
+		cout << endl << endl;
+	}
+}
---
+++ Task2/Task2/Task2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int matrix[500][500];
+	int size;
+
+	cout << "Введите размер квадратной матрицы" << endl;
+
+	cin >> size;
+
+	if (size <= 0)
+	{
+		cout << "Невалидное значение. Должно быть > 0" << endl;
+		return 0;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+			matrix[i][j] = 0;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		int next_number = size;
+		for (int j = i; j > -1; j--)
+		{
+			matrix[i][j] = next_number;
+			next_number--;
+		}
+	}
+
+	cout << "Ваша матрица:" << endl << endl;
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+			cout << matrix[i][j] << " ";
+
+		cout << endl << endl;
+	}
+}
